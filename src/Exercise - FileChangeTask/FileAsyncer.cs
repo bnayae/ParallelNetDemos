912d838e1006593c9926f9f3d63121f8ead2c1a6
bnayae <Bnayanet@hotmail.com>
2019-06-13 11:38:09 +0300

    file watcher

---
+++ src/Exercise - FileChangeTask/FileAsyncer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bnaya.Samples
+{
+    public class FileAsyncer
+    {
+        public Task<string> WatchFileDeletionAsync(string path)
+        {
+            // TODO: watch for deletion (return the file name)
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Exercise - FileChangeTask/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bnaya.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var fsw = new FileSystemWatcher("Files");
+            //fsw.EnableRaisingEvents = true;
+            //fsw.Deleted += (e, a) => Console.WriteLine(a.Name);
+
+            var w = new FileAsyncer();
+            Task<string> t = w.WatchFileDeletionAsync("Files");
+            while (!t.IsCompleted)
+            {
+                Console.Write(".");
+                Thread.Sleep(100);
+            }
+            Console.WriteLine(t.Result);
+
+            Console.ReadKey();
+        }
+    }
+}
